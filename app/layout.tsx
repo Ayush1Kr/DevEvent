d0cbc2dc942ac38273048055c36a14b3e4b75089
Ayush Kumar <a4ayush2004@gmail.com>
2026-07-11 16:08:17 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,58 @@
+import type { Metadata } from "next";
+import { Schibsted_Grotesk, Martian_Mono } from "next/font/google";
+import "./globals.css";
+import LightRays from '@/component/LightRays'
+import Navbar from '@/component/Navbar'
+
+
+const schibstedGrotesk = Schibsted_Grotesk({
+  variable: "--font-schibsted-grotesk",
+  subsets: ["latin"],
+});
+
+const martianMono = Martian_Mono({
+  variable: "--font-martian-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "DevEvent",
+  description: "The Hub for Every Dev Event You Mustn't Miss.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${schibstedGrotesk.variable} ${martianMono.variable} min-h-screen antialiased`}
+    >
+      <body className="min-h-full flex flex-col">
+        <Navbar/>
+        <div className="absolute inset-0 top-0 z-[-1] min-h-screen">
+          <LightRays
+              raysOrigin="top-center"
+              raysColor="#5dfeca"
+              raysSpeed={0.5}
+              lightSpread={0.9}
+              rayLength={1.4}
+              followMouse={true}
+              mouseInfluence={0.05}
+              noiseAmount={0}
+              distortion={0}
+              className="custom-rays"
+              pulsating={false}
+              fadeDistance={1}
+              saturation={1}
+          />
+        </div>
+        <main>
+            {children}
+        </main>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import ExploreBtn from '@/component/ExploreBtn'
+import EventCard from '@/component/EventCard'
+import {events} from '@/lib/constants'
+
+
+
+const Page = () => {
+    return (
+        <section>
+            <h1 className="text-center">The Hub for Every Dev <br/> Event You Can't Miss</h1>
+            <p className='text-center mt-5'>Hackathons, Meetups and Conferences, All in One Place</p>
+
+            <ExploreBtn/>
+
+            <div className='mt-20 space-y-7'>
+                <h3>Featured Events</h3>
+                <ul className='events'>
+                    {events.map((item) => (
+                        <li key={item.title}><EventCard {...item}/></li>
+                    ))}
+                </ul>
+            </div>
+        </section>
+    )
+}
+export default Page
